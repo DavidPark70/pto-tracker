91d74447d8148bcfb23677a801259a26e5df6b7f
dpar <yedarm.park@panasonic.aero>
2023-10-30 17:18:11 -0700

    updates

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -3,25 +3,59 @@ import { DataGrid } from '@mui/x-data-grid';
 
 const AdminDashboard = () => {
 
+    const getDaysInMonth = (year, month) => {
+        let date = new Date(year, month, 0);
+        return date.getDate();
+    }
+
     const today = new Date();
-    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-    const remainingDays = Math.ceil((lastDayOfMonth - today) / (1000 * 60 * 60 * 24));
-    const monthViewDays = 23;
+    const todayYear = today.getFullYear();
+    const todayMonth = today.getMonth();
+    const columnLenght = getDaysInMonth(todayYear, todayMonth+1);
 
-    console.log(remainingDays);
-
-    let columns = [
-        {field: '30'},
-        {field: '31'}
-    ]
-
+    let days = {
+        0: "Sun",
+        1: "Mon",
+        2: "Tue",
+        3: "Wed",
+        4: "Thu",
+        5: "Fri",
+        6: "Sat"
+    }
+    let columns = [];
     let rows = [
         {}
-    ]
+    ];
 
+    for (var i = 1; i <= columnLenght; i++) {
+        let tempDate = new Date(todayYear, todayMonth, i);
+        console.log(tempDate);
+        columns.push({
+            field: String(i) + ' ' + days[tempDate.getDay()],
+            sortable: false
+        })
+    }
+
+    let testCol = [
+        {field: '1', sortable: false},
+        {field: '2', sortable: false},
+        {field: '3', sortable: false},
+        {field: '4', sortable: false},
+        {field: '5', sortable: false},
+    ]
+    let testRow = [
+        {id: 1, 1: 'y'}
+    ]
     return (
-        <div>AdminDashboard</div>
+        <DataGrid 
+            rows={testRow} 
+            columns={columns} 
+            disableColumnMenu={true}    
+            disableRowSelectionOnClick={true}
+            hideFooterPagination={true}
+            hideFooter={true}
+        />
     )
 }
 
-export default AdminDashboard
+export default AdminDashboard;
